212ab9b1661e3d618c66f720d1f205e16f27410e
Fellipe Marcel <fmrclms@gmail.com>
2023-07-02 18:39:15 -0300

    removed react type animation lib

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types"
 import '../Button/styles.css';
 
-const Button = ({ btnContent, btnId, btnType }) => {
+const Button = ({ btnContent, btnId, btnType, onClick }) => {
     return (
         <button
         id={ btnId }
         type={ btnType }
+        onClick={onClick}
         >
             <span className="btn-content">{ btnContent }</span>
         </button>
@@ -16,6 +17,7 @@ Button.propTypes = {
   btnContent: PropTypes.string,
   btnId: PropTypes.string,
   btnType: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Button;
